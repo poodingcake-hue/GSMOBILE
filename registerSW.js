d7888f424ee2931af231ebfa379b429eec49cdef
poodingcake-hue <poodingcake-hue@users.noreply.github.com>
2026-06-05 19:41:13 +0000

    deploy: efaf9c8c4373a82b491a9380e3af7e1e8c5a4f70

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/GSMOBILE/sw.js', { scope: '/GSMOBILE/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
